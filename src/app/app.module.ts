89d9684b2b11a503179f8fea150237e53e3a428c
Nina Pantelic <nina.pantelic20@gmail.com>
2023-05-31 18:19:15 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent],
+
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(),
+		AppRoutingModule,
+		provideFirebaseApp(() => initializeApp(environment.firebase)),
+		provideAuth(() => getAuth()),
+		provideFirestore(() => getFirestore()),
+		provideStorage(() => getStorage())
+	],
+	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+	bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+		apiKey: '',
+		authDomain: '',
+		projectId: '',
+		storageBucket: '',
+		messagingSenderId: '',
+		appId: ''
+	}
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB8eWIz10m8B7d-7rOOpM984iS4mbEkF-U",
+  authDomain: "domaciionic.firebaseapp.com",
+  projectId: "domaciionic",
+  storageBucket: "domaciionic.appspot.com",
+  messagingSenderId: "1079963669668",
+  appId: "1:1079963669668:web:1b350588c7bb5cdac3c231"
+};
+
 
 /*
  * For easier debugging in development mode, you can import the following file
